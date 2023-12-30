b96331b53576f14cf68ce2462516e5e8d0fd9bce
Wei Lun <weilun9320@hotmail.com>
2023-12-30 17:53:39 +0800

    Added handle login function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,40 @@
-import { Container, Image } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Alert, Container, Image } from "react-bootstrap";
+import { useLocation } from "react-router-dom";
 
 export default function Home() {
+  const [loginSuccessMessage, setLoginSuccessMessage] = useState("");
+  const [isAuthorize, setIsAuthorize] = useState("");
+  const location = useLocation();
+
+  useEffect(() => {
+    const successMessage = location.state?.successMessage || "";
+    setLoginSuccessMessage(successMessage);
+    const authorizeUsername = location.state?.username || "";
+    setIsAuthorize(authorizeUsername);
+
+    const timeoutId = setTimeout(() => {
+      setLoginSuccessMessage("");
+    }, 5000);
+
+    return () => clearTimeout(timeoutId);
+  }, [location.state]);
+
   return (
-    <div className="position-relative">
-      <Image alt="home-bg" className="w-100 object-fit-cover" loading="lazy" src="./assets/home-bg.jpg" />
-      <Container className="position-absolute top-50 start-50 translate-middle text-center">
-        <h1 className="text-light title">
-          Home Page
-        </h1>
-      </Container>
-    </div>
+    <>
+      {loginSuccessMessage && (
+        <Alert className="text-center mb-0" variant="success">
+          {loginSuccessMessage}
+        </Alert>
+      )}
+      <div className="position-relative">
+        <Image alt="home-bg" className="w-100 object-fit-cover" loading="lazy" src="./assets/home-bg.jpg" />
+        <Container className="position-absolute top-50 start-50 translate-middle text-center">
+          <h1 className="text-light title">
+            {isAuthorize ? `Welcome, ${isAuthorize}` : "Home Page"}
+          </h1>
+        </Container>
+      </div>
+    </>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,42 +1,137 @@
-import { useState } from "react";
-import { Button, Card, Container, Form, Image } from "react-bootstrap";
-export default function Login() {
+import { useEffect, useState } from "react";
+import { Alert, Button, Card, Container, Form, Image, Spinner } from "react-bootstrap";
+import { useLocation, useNavigate } from "react-router-dom";
+
+export default function Login({ API_URL }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [responseMessage, setResponseMessage] = useState("");
+  const [signUpSuccessMessage, setSignUpSuccessMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const successMessage = location.state?.successMessage || "";
+    setSignUpSuccessMessage(successMessage);
+
+    const timeoutId = setTimeout(() => {
+      setSignUpSuccessMessage("");
+    }, 5000);
+
+    return () => clearTimeout(timeoutId);
+  }, [location.state]);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    setResponseMessage("");
+    setIsLoading(true);
+
+    if (!username || !password) {
+      setResponseMessage("Please fill up all fields.");
+      setIsLoading(false);
+
+      return;
+    }
+
+    fetch(`${API_URL}/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password })
+    })
+      .then(res => {
+        if (res.ok) {
+          return res.json().then(data => {
+            return data.token;
+          });
+        }
+        else {
+          return res.json().then(data => data.token);
+        }
+      })
+      .then(authData => {
+        if (authData) {
+          fetch(`${API_URL}/username`, {
+            method: "GET",
+            headers: { "Authorization": authData }
+          })
+            .then(res => {
+              if (res.ok) {
+                return res.json().then(data => data.username);
+              }
+            })
+            .then(updatedData => {
+              navigate("/", { state: { successMessage: "User login successfully", username: updatedData } });
+            })
+            .catch(error => console.error(error));
+        }
+        else {
+          setResponseMessage("Username or password incorrect");
+          setIsLoading(false);
+        }
+      })
+      .catch(error => console.error(error));
+  }
 
   return (
-    <div className="position-relative">
-      <Image alt="mountain-bg" className="w-100 object-fit-cover" loading="lazy" src="./assets/mountain-bg.jpg" />
-      <Container className="position-absolute top-0 start-50 translate-middle-x mt-3 mt-md-5">
-        <Card className="mx-auto my-3 border-0">
-          <Card.Body>
-            <h2 className="mb-3">Login</h2>
-            <Form>
-              <Form.Group className="mb-3" controlId="username">
-                <Form.Label>Username</Form.Label>
-                <Form.Control
-                  onChange={e => setUsername(e.target.value)}
-                  placeholder="Enter username"
-                  type="text"
-                  value={username}
-                />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  onChange={e => setPassword(e.target.value)}
-                  placeholder="Password"
-                  type="password"
-                  value={password}
-                />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Login
-              </Button>
-            </Form>
-          </Card.Body>
-        </Card>
-      </Container>
-    </div>
+    <>
+      {signUpSuccessMessage && (
+        <Alert className="text-center mb-0" variant="success">
+          {signUpSuccessMessage}
+        </Alert>
+      )}
+      <div className="position-relative">
+        <Image alt="mountain-bg" className="w-100 object-fit-cover" loading="lazy" src="./assets/mountain-bg.jpg" />
+        <Container className="position-absolute top-0 start-50 translate-middle-x mt-3 mt-md-5">
+          <Card className="mx-auto my-3 border-0">
+            <Card.Body>
+              <h2 className="mb-3">Login</h2>
+              <Form onSubmit={handleLogin}>
+                <Form.Group className="mb-3" controlId="username">
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control
+                    onChange={e => setUsername(e.target.value)}
+                    placeholder="Enter username"
+                    type="text"
+                    value={username}
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="password">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    onChange={e => setPassword(e.target.value)}
+                    placeholder="Password"
+                    type="password"
+                    value={password}
+                  />
+                </Form.Group>
+                {responseMessage &&
+                  <p className="text-danger">{responseMessage}</p>
+                }
+                {isLoading ? (
+                  <>
+                    <Spinner
+                      animation="border"
+                      as="span"
+                      className="me-2"
+                      size="sm"
+                      role="status"
+                      variant="primary"
+                    />
+                    <span>Loading...</span>
+                  </>
+                )
+                  : (
+                    <Button variant="primary" type="submit">
+                      Login
+                    </Button>
+                  )}
+              </Form>
+            </Card.Body>
+          </Card>
+        </Container>
+      </div>
+    </>
   );
 }
